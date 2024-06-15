e86bbbb203ed7e9a26b8626ba208c13c5de66cbe
JanKocanda <bun.noob.bun@gmail.com>
2024-06-15 23:51:15 +0200

    added static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
