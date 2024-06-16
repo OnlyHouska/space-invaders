0a1a13804de3cfbed3f1ef1222cb5896d4313203
JanKocanda <bun.noob.bun@gmail.com>
2024-06-16 23:12:23 +0200

    added update notes -fix

---
+++ src/routes/components/update_notes.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import Close from './close.svelte';
+	import Move from './move.svelte';
+	import { version } from '$lib/global';
+	import updateNotes from '$lib/update_notes.json';
+
+	export let close: () => void;
+	let board: HTMLDivElement;
+
+	let strippedVersion = version.replace('v. ', '');
+</script>
+
+<div bind:this={board} class="popup_window">
+	<Close {close} />
+	<h1>Update notes</h1>
+	<div class="flex flex-col mt-3 gap-3">
+		{#each Object.keys(updateNotes) as ver}
+			<div class="flex flex-col gap-2">
+				<h2 class="version">
+					Version {ver}
+					{#if ver == strippedVersion}(Latest){/if}:
+				</h2>
+				<div>
+					{#each updateNotes[ver] as note}
+						<p>{note}</p>
+					{/each}
+				</div>
+			</div>
+		{/each}
+	</div>
+	<Move {board} />
+</div>
+
+<style>
+	.version {
+		color: rgba(255, 255, 255, 0.6);
+		text-decoration: underline;
+	}
+	.version ~ div,
+	.version {
+		font-size: x-small;
+	}
+</style>
---
+++ src/routes/components/version.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import note from '$lib/assets/note.svg';
+	import Window from '../Window.svelte';
+
+	import { Type, version } from '$lib/global';
+
+	let board: HTMLElement;
+	let notesButton: HTMLButtonElement;
+
+	let showUpdateNotes: boolean = false;
+	function toggleUpdateNotes() {
+		showUpdateNotes = !showUpdateNotes;
+		notesButton.blur();
+	}
+</script>
+
+<div
+	class="absolute bottom-0 right-0 mr-2 mb-2 flex flex-row gap-2 justify-center items-center"
+	bind:this={board}
+>
+	<button bind:this={notesButton} on:click={toggleUpdateNotes}>
+		<img src={note} alt="update notes" class="w-7 h-7 opacity-30 hover:opacity-60 transition-all" />
+	</button>
+	<p class="text-xs text-white/30">{version}</p>
+</div>
+{#if showUpdateNotes}
+	<Window popupType={Type.updateNotes} close={toggleUpdateNotes} />
+{/if}
