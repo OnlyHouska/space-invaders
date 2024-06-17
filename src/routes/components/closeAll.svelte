0184f5d5f5711406b6a13b5a537073cfaf9fb1bf
JanKocanda <bun.noob.bun@gmail.com>
2024-06-17 11:02:11 +0200

    changed window positions for smaller devices

---
+++ src/routes/components/closeAll.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import {
+		toggleGamePopup,
+		toggleHighscoresPopup,
+		toggleSettingsPopup,
+		toggleUpdateNotes
+	} from '../../lib/toggleWindows';
+
+	function closeAll(): void {
+		toggleGamePopup(null, true);
+		toggleSettingsPopup(null, true);
+		toggleUpdateNotes(null, true);
+		toggleHighscoresPopup(null, true);
+	}
+</script>
+
+<button on:click={closeAll} title="Close all windows" class="text-xl order-1"> x </button>
